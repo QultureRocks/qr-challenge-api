6440fd622dd3b2b57353ed194f01cf411054f582
Manuel Puyol <manuelpuyol@gmail.com>
2018-11-13 14:27:38 -0200

    :shirt:

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 
 ENV['RAILS_ENV'] ||= 'test'
 
-require File.expand_path('../../config/environment', __FILE__)
+require File.expand_path('../config/environment', __dir__)
 require 'spec_helper'
 require 'rspec/rails'
 
